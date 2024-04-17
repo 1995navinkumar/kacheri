f51de91648907fe2f4b7b5ffeaf6e09a85494123
1995navinkumar <navin.kumar@razorpay.com>
2024-04-17 19:07:51 +0530

    add faro

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { initializeFaro } from "@grafana/faro-web-sdk";
+import { version } from "../../package.json";
+
+initializeFaro({
+  url: "https://gently-concise-dogfish.ngrok-free.app/faro-grafana/collect",
+  app: {
+    name: "kacheri",
+    version,
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
